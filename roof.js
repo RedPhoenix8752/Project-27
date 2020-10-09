db264c7a92a30f7f685bd2593180f293b9ef464b
pheonixmaster12 <71676359+pheonixmaster12@users.noreply.github.com>
2020-10-09 10:12:19 -0600

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,26 @@
+class Roof {
+    constructor(x, y, width, height) {
+      var options = {
+          isStatic: false,
+          restitution:0.3,
+          friction:0.5,
+          density:1.2
+      }
+      
+      this.image = loadImage ("sprites/paper.png");
+      this.width = width;
+      this.height = height;
+      
+      
+
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill(255);
+      rect(pos.x, pos.y, this.width, this.height);
+      pop();
+    }
+  };
+  
